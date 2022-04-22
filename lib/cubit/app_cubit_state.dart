d8423d14ed99c42bdea968b12e83933e2ef0c30b
RafiaChy <rafiachowdhury24@gmail.com>
2022-04-22 18:08:32 +0600

    created a cubit

---
+++ lib/cubit/app_cubit_state.dart
@@ -0,0 +1,10 @@
+part of 'app_cubit_cubit.dart';
+
+abstract class AppCubitState extends Equatable {
+  const AppCubitState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class AppCubitInitial extends AppCubitState {}
